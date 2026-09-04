34a15ed6b2678278e7c51becc09a458ec93050fa
Behruz <baxromov@Behruzs-MacBook-Neo.local>
2026-09-05 01:44:57 +0500

    Fix AAR metadata check by enforcing compileSdk 34 on subprojects

--- starpath/android/build.gradle.kts
+++ starpath/android/build.gradle.kts
@@ -19,6 +19,19 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val androidExt = project.extensions.findByName("android")
+            if (androidExt is com.android.build.gradle.BaseExtension) {
+                if (androidExt.compileSdkVersion == null || androidExt.compileSdkVersion == "android-33" || (androidExt.compileSdkVersion?.startsWith("android-") == true && (androidExt.compileSdkVersion?.substring(8)?.toIntOrNull() ?: 0) < 34)) {
+                    androidExt.compileSdkVersion(34)
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
